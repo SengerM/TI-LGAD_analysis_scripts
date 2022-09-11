1de9f43dcc04134023fe3bd6a9f399d64e16db6e
Matias Senger <m.senger@hotmail.com>
2022-09-11 15:44:17 +0200

    Implemented `normalization_factors_for_amplitude`

--- utils.py
+++ utils.py
@@ -7,6 +7,9 @@ from huge_dataframe.SQLiteDataFrame import load_whole_dataframe # https://github
 from pathlib import Path
 import numpy
 import warnings
+import grafica.plotly_utils.utils as graficas_px_utils
+
+graficas_px_utils.set_my_template_as_default()
 
 def create_a_timestamp():
 	time.sleep(1) # This is to ensure that no two timestamps are the same.
@@ -166,6 +169,122 @@ def resample_by_events(data_df):
 		warnings.warn(f'There was an automatic cast by Pandas while running `pivot` to resample, please check this because it may not broke the code but severely affect performance after resampling.')
 	return resampled_df
 
+def normalize_data(data_df, normalization_factors_df):
+	"""Given normalization factors in the form
+	```
+					  Amplitude (V)                            
+							 offset divide_by_this_to_normalize
+	n_channel n_pulse                                          
+	1         1            0.013011                    0.327003
+			  2            0.012813                    0.240779
+	4         1            0.013397                    0.343928
+			  2            0.013309                    0.253097
+	```
+	and data e.g.
+	```
+											Amplitude (V) normalized
+	n_position n_trigger n_channel n_pulse                          
+	0          0         1         1                        0.012532
+								   2                        0.013547
+						 4         1                        0.017058
+								   2                        0.015573
+			   1         1         1                        0.012446
+	...                                                          ...
+	333        109       4         2                        0.012202
+			   110       1         1                        0.012348
+								   2                        0.012273
+						 4         1                        0.012907
+								   2                        0.012770
+
+	[148296 rows x 1 columns]
+	```
+	this function returns a new data frame with the columns present in 
+	`normalization_factors_df` (in this example `"Amplitude (V)"`) but
+	normalized.
+	"""
+	normalized = pandas.DataFrame(index=data_df.index)
+	for col in set(normalization_factors_df.columns.get_level_values(0)):
+		normalized[f'{col} normalized'] = data_df[col]
+	normalized.reset_index(inplace=True, drop=False)
+	normalized.set_index(normalization_factors_df.index.names, inplace=True)
+	normalized.sort_index(inplace=True)
+	for col in set(normalization_factors_df.columns.get_level_values(0)):
+		for i in normalization_factors_df.index:
+			normalized.loc[i,f'{col} normalized'] -= normalization_factors_df.loc[i,(col,'offset')]
+			normalized.loc[i,f'{col} normalized'] /= normalization_factors_df.loc[i,(col,'divide_by_this_to_normalize')]
+	normalized.reset_index(inplace=True, drop=False)
+	normalized.set_index(data_df.index.names, inplace=True)
+	return normalized
+
+def normalization_factors_for_amplitude(bureaucrat:RunBureaucrat, approximate_window_size_meters:float, approximate_laser_size_meters:float, force:bool=False):
+	"""Calculates the normalization factors for the amplitude and charge
+	of each channel such that they go from 0 to 1."""
+	bureaucrat.check_these_tasks_were_run_successfully(['TCT_1D_scan','parse_waveforms','tag_channels_left_right'])
+	
+	if force==False and bureaucrat.was_task_run_successfully('normalization_factors_for_amplitude'):
+		return
+	
+	with bureaucrat.handle_task('normalization_factors_for_amplitude') as employee:
+		data_df = load_parsed_from_waveforms_and_measured_data_in_TCT_1D_scan(bureaucrat)
+		channel_positions = pandas.read_pickle(bureaucrat.path_to_directory_of_task('tag_channels_left_right')/'channels_position.pickle')
+		distance = generate_distance_vs_n_position(data_df)
+		
+		data_df = data_df[['Amplitude (V)']]
+		data_df = data_df.join(distance, on='n_position')
+		
+		distance_left_window_ends = distance.mean()-approximate_laser_size_meters
+		distance_left_window_begins = distance.mean()-approximate_window_size_meters/2+approximate_laser_size_meters
+		distance_right_wndow_begins = distance.mean()+approximate_laser_size_meters
+		distance_right_window_ends = distance.mean()+approximate_window_size_meters/2-approximate_laser_size_meters
+		
+		normalization_data = []
+		for n_channel in set(data_df.index.get_level_values('n_channel')):
+			for n_pulse in set(data_df.index.get_level_values('n_pulse')):
+				use_this_data = data_df.query(f'n_channel=={n_channel}').query(f'n_pulse=={n_pulse}')
+				if channel_positions.loc[n_channel] == 'left':
+					data_where_I_expect_amplitude_to_be_maximum = use_this_data.query(f'`Distance (m)`<{distance_left_window_ends}').query(f'`Distance (m)`>{distance_left_window_begins}')
+					data_where_I_expect_amplitude_to_be_minimum = use_this_data.query(f'`Distance (m)`>{distance_right_wndow_begins}')
+				else: # right channel
+					data_where_I_expect_amplitude_to_be_maximum = use_this_data.query(f'`Distance (m)`>{distance_right_wndow_begins}').query(f'`Distance (m)`<{distance_right_window_ends}')
+					data_where_I_expect_amplitude_to_be_minimum = use_this_data.query(f'`Distance (m)`<{distance_left_window_ends}')
+				amplitude_offset = data_where_I_expect_amplitude_to_be_minimum['Amplitude (V)'].median()
+				amplitude_divide_by_this_to_normalize = (data_where_I_expect_amplitude_to_be_maximum['Amplitude (V)']-amplitude_offset).median()
+				df = pandas.DataFrame({'n_channel':n_channel}, index=[0]).set_index('n_channel')
+				df = pandas.DataFrame(
+					{
+						'Amplitude (V) offset': amplitude_offset,
+						'Amplitude (V) divide_by_this_to_normalize': amplitude_divide_by_this_to_normalize,
+						'n_channel': n_channel,
+						'n_pulse': n_pulse,
+					},
+					index = [0],
+				).set_index(['n_channel','n_pulse'])
+				df.columns = pandas.MultiIndex.from_product([['Amplitude (V)'],['offset','divide_by_this_to_normalize']])
+				normalization_data.append(df)
+		normalization_data = pandas.concat(normalization_data)
+		
+		normalization_data.to_pickle(employee.path_to_directory_of_my_task/'normalization_data.pickle')
+		
+		averaged_in_position_df = normalize_data(data_df, normalization_data).groupby(['n_position','n_channel','n_pulse']).agg([numpy.nanmedian, kMAD])
+		averaged_in_position_df.columns = [f'{col[0]} {col[1]}' for col in averaged_in_position_df.columns]
+		averaged_in_position_df = averaged_in_position_df.join(distance, on='n_position')
+		averaged_in_position_df = averaged_in_position_df.join(channel_positions, on='n_channel')
+		
+		fig = graficas_px_utils.line(
+			data_frame = averaged_in_position_df.reset_index().sort_values(['channel_position','n_position']),
+			x = 'Distance (m)',
+			y = 'Amplitude (V) normalized nanmedian',
+			error_y = 'Amplitude (V) normalized kMAD',
+			error_y_mode = 'bands',
+			color = 'channel_position',
+			line_dash = 'n_pulse',
+			title = f'Normalized amplitude<br><sup>Run: {bureaucrat.run_name}</sup>',
+		)
+		fig.write_html(
+			employee.path_to_directory_of_my_task/'amplitude normalized.html',
+			include_plotlyjs = 'cdn',
+		)
+
 if __name__=='__main__':
 	import argparse
 
@@ -187,5 +306,9 @@ if __name__=='__main__':
 	args = parser.parse_args()
 	
 	Enrique = RunBureaucrat(Path(args.directory))
-	tag_channels_left_right(Enrique)
+	normalization_factors_for_amplitude(
+		Enrique,
+		approximate_window_size_meters = 250e-6,
+		approximate_laser_size_meters = 11e-6,
+	)
 	
